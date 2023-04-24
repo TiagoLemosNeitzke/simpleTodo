8a5b3b208db30a7ac73c17833c398245f13d5993
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-04-24 16:05:35 -0400

    refactor in create user to add a column phone number correctly to use in notification

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,14 +33,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'phone' => ['required', 'string', 'min:11', 'max:13', 'unique:'.User::class],
+            'phone_number' => ['required', 'string', 'min:11', 'max:13', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'phone' => $request->phone,
+            'phone_number' => $request->phone_number,
             'password' => Hash::make($request->password),
         ]);
 
