68c74a2b5f84aa1940d454b65f1f5e31b2c4c72f
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-05-08 14:37:10 -0400

    finished layout homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,8 +54,8 @@
             <p class="text-white font-thin text-xs md:text-sm  justify-center flex mt-6">Seja avisado quando haver tarefas para realizar</p>
         </div>
         <div class="mt-4 md:mt-8 flex flex-col md:flex-row md:justify-around md:w-80">
-            <a href="" class="bg-white text-[#06604e] font-medium px-4 py-1 hover:bg-slate-200">Saiba mais</a>
-            <a href="{{ route('register') }}" class="mt-4 md:mt-0 text-white font-medium px-4 py-1  bg-[#de8609] hover:bg-[#cf7d08] duration-150" >Registre-se</a>
+            <a href="#readMore" class="bg-white text-[#06604e] font-medium px-4 py-1 hover:bg-slate-200">Saiba mais</a>
+            <a href="{{ route('register') }}" class="mt-4 md:mt-0 text-white font-medium px-4 py-1  bg-[#de8609] hover:bg-[#cf7d08] duration-150" >Teste Grátis</a>
         </div>
     </div>
 </header>
@@ -86,7 +86,12 @@
         </div>
 
         <div class="flex flex-col md:flex-row justify-around items-center w-full md:mt-12">
-            <div class="w-48 md:w-1/3 mt-6 h-48 border-[#06604e] border-[0.625px]"></div>
+            <div class="flex flex-col justify-center items-center w-48 md:w-1/3 mt-6 h-48 border-[#06604e] border-[0.625px]">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-[#06604e]">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+                </svg>
+                <p class="font-thin text-[14px] md:mt-4 md:font-normal md:text-sm">Olá! Você tem tarefas agendadas para hoje.</p>
+            </div>
             <div class="flex flex-col text-start">
                 <h2 class="text-[#06604e] font-bold mt-6 md:text-lg">Receba notificações</h2>
                 <p class="font-thin text-[14px] md:mt-4 md:font-normal md:text-sm md:w-80">Você pode criar listas do que quiser, nós enviaremos uma mensagem SMS no seu celular e um email sempre que houver tarefas a serem realizadas.</p>
@@ -94,10 +99,10 @@
         </div>
     </div>
 
-    <div class="bg-slate-200 w-full mt-20 flex flex-col items-center space-y-12">
-        <h4 class="text-[#06604e] font-black md:text-3xl md:mt-12">Porque Todo List?</h4>
+    <div id="readMore" class="bg-slate-200 w-full mt-20 flex flex-col items-center space-y-12">
+        <h4 class="text-[#06604e] font-black md:text-3xl md:mt-12">Porque Todo?</h4>
 
-        <div class="flex flex-row space-x-20">
+        <div class="flex flex-col md:flex-row md:space-x-20">
             <div class="flex flex-col justify-center items-center mt-12 w-48 space-x-4">
                 <div class="p-4 bg-[#de8609] rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
@@ -128,7 +133,7 @@
             </div>
         </div>
 
-        <div class="flex flex-row space-x-20">
+        <div class="flex flex-col md:flex-row md:space-x-20">
             <div class="flex flex-col justify-center items-center mt-12 w-48 space-x-4">
                 <div class="p-4 bg-[#de8609] rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
@@ -151,23 +156,73 @@
             </div>
             <div class="flex flex-col justify-center items-center mt-12 w-48 space-x-4">
                 <div class="p-4 bg-[#de8609] rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="text-white" class="w-6 h-6 fill-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                     </svg>
+
                 </div>
-                <h5 class="text-[#06604e] text-lg">Comunidade</h5>
-                <p class="text-xs mt-2">Ao se juntar a nós, você também obtém acesso à nossa comunidade global de brogrammers no Slack. Obtenha ajuda e saiba mais sobre as atualizações mais recentes..</p>
+                <h5 class="text-[#06604e] text-lg">Tarefa realizada</h5>
+                <p class="text-xs mt-2">Marque suas tarefas como realizadas. Mantenha um histórico de tudo o que você realizou.</p>
             </div>
         </div>
-        <div class="text-center flex flex-col pb-12">
+        <div class="text-center flex flex-col pb-24">
             <p class="text-slate-400 text-lg">Pronto para começar?</p>
             <a href="{{ route('register') }}" class="mt-4 text-white font-medium px-4 py-1  bg-[#de8609] hover:bg-[#cf7d08] duration-150" >Registre-se</a>
         </div>
     </div>
 </main>
 
-<footer class="w-full bg-[#06604e] h-40">
+<footer class="w-full bg-[#06604e] h-80 absolute flex flex-col items-center justify-center pb-4">
+    <div class="bg-[#de8609] relative mt-[-100px] w-8/12 h-40 text-start items-center flex flex-col md:flex-row md:justify-around justify-center">
+        <div>
+            <h4 class="text-[#06604e] font-bold md:text-2xl">Organize seu dia.</h4>
+            <p class="text-xs mt-1">Teste Grátis por 30 dias.</p>
+        </div>
+        <a href="{{ route('register') }}" class="bg-white text-[#06604e] text-xs md:text-base px-4 py-1 mt-4 font-semibold hover:bg-slate-200">Comece agora!</a>
+    </div>
+    <div class="flex flex-col md:flex-row justify-between w-8/12 mt-12">
+        <div>
+            <x-application-logo class="w-8 h-8 fill-white text-gray-500" />
+            <p class="text-white text-xs font-light"> Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
+            <p class="text-white text-xs font-light">&copy; {{ date_format(now(), 'Y') }} Todo Company</p>
+            <a href="mailto:tiago.neitzke@yahoo.com"  class="text-white text-xs font-light hover:underline">tiago.neitzke@yahoo.com</a>
+        </div>
+        <div class="mt-2 md:mt-0">
+            <p class="text-white text-xs font-light mb-2">Não encontrou o que procurava?</p>
+            <a href="mailto:tiago.neitzke@yahoo.com"  class="text-white text-xs font-normal hover:bg-[#cf7d08] bg-[#de8609] py-1 px-2">Envie um e-mail</a>
+        </div>
 
+    </div>
+    <div class="flex flex-row justify-between w-8/12 mt-6">
+        <div>
+            <p class="flex text-white text-[12px] font-light">
+                Feito com
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="ml-1 w-3 h-3 text-pink-700">
+                    <path d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z" />
+                </svg>
+            </p>
+        </div>
+
+        <div class="flex justify-between w-1/12">
+            <a href="https://www.linkedin.com/in/tiago-lemos-neitzke/" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin text-sky-600 hover:text-white" viewBox="0 0 16 16">
+                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                </svg>
+            </a>
+
+            <a href="https://twitter.com/LemosNeitzke" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter text-sky-600 hover:text-white" viewBox="0 0 16 16">
+                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                </svg>
+            </a>
+
+            <a href="https://wa.me/5567999082296" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp  text-emerald-600 hover:text-white" viewBox="0 0 16 16">
+                    <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                </svg>
+            </a>
+        </div>
+    </div>
 </footer>
 
 </body>
